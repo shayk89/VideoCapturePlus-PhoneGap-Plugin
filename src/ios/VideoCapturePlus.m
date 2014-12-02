506abbef758ae1edbe173ac0823b00c935ae7933
shayk89 <shay@Shays-Mac.local>
2014-12-01 16:52:09 -0800

    .

--- src/ios/VideoCapturePlus.m
+++ src/ios/VideoCapturePlus.m
@@ -306,6 +306,7 @@
         NSData *videoData = [NSData dataWithContentsOfURL:url];
         PFFile *videoFile = [PFFile fileWithName:@"video.mp4" data:videoData];
         [videoFile saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error){
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Thrift Karma" message:error.description delegate: nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
             [item setObject:videoFile forKey:@"video"];
             [item save];
         
